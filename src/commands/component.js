59f89a08ecc62974694802fb76db904dd1aaed11
Ahmet Karadeniz <ahmetkaradeniz.ce@gmail.com>
2024-03-23 20:05:17 +0300

    command component

--- src/commands/component.js
+++ src/commands/component.js
@@ -12,31 +12,31 @@ export default (name) => {
 
     const settings = getSettings()
 
-    // // Component Path Control
-    // let componentPath = resolve(settings.path.component)
-    // if (!existsSync(componentPath)) {
-    //     console.error(chalk.red('Path not found'))
-    //     process.exit(1)
-    // }
-
-    // let fileName = name
-
-    // // Name Path Control
-    // if (name.includes('/')) {
-    //     const paths = name.split('/')
-    //     for (let i = 0; i < paths.length - 1; i++) {
-    //         const tempPath = join(componentPath, paths[i])
-    //         if (!existsSync(tempPath)) {
-    //             mkdirSync(tempPath)
-    //         }
-    //         componentPath = tempPath
-    //     }
-    //     fileName = paths[paths.length - 1]
-    // }
-
-    // let componentStub = readFileSync(join(dirname(fileURLToPath(import.meta.url)), '..', 'stubs', 'component', settings.app.language, `${settings.app.api}.stub`), 'utf-8')
-
-    // componentStub = componentStub.replace(/{{APP_CSS}}/g, settings.app.style)
+    // Component Path Control
+    let componentPath = resolve(settings.path.component)
+    if (!existsSync(componentPath)) {
+        console.error(chalk.red(`Path not found ${settings.path.component}`))
+        process.exit(1)
+    }
     
-    // writeFileSync(join(componentPath, `${fileName}.vue`), componentStub, { flag: 'w' })
+    let fileName = name
+
+    // Name Path Control
+    if (name.includes('/')) {
+        const paths = name.split('/')
+        for (let i = 0; i < paths.length - 1; i++) {
+            const tempPath = join(componentPath, paths[i])
+            if (!existsSync(tempPath)) {
+                mkdirSync(tempPath)
+            }
+            componentPath = tempPath
+        }
+        fileName = paths[paths.length - 1]
+    }
+
+    let componentStub = readFileSync(join(dirname(fileURLToPath(import.meta.url)), '..', 'stubs', 'component', settings.app.language, `${settings.app.api}.stub`), 'utf-8')
+
+    componentStub = componentStub.replace(/{{APP_CSS}}/g, settings.app.style)
+
+    writeFileSync(join(componentPath, `${fileName}.vue`), componentStub, { flag: 'w' })
 }
