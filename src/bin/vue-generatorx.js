0e816a5e49a291dda6ead91252726c4c7cfa52d1
Ahmet Karadeniz <ahmetkaradeniz.ce@gmail.com>
2024-03-23 19:55:11 +0300

    local config update

--- src/bin/vue-generatorx.js
+++ src/bin/vue-generatorx.js
@@ -14,5 +14,5 @@ program
   .description(packageJson.description)
   .version(packageJson.version)
   .usage('<command> [options]')
-  .command('init', 'Create a local settings file (vgx.config.{js,ts})')
+  .command('init', 'Create a local settings file (vgx.config.json)')
   .parse(process.argv)
